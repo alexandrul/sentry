12aa054aaceb8bd86fe161b171fbc1ccd86f1c8c
Eduard-Cristian Stefan <alexandrul.ct@gmail.com>
2018-10-20 16:05:35 +0200

    Added guest role

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1172,6 +1172,19 @@ SENTRY_DEFAULT_ROLE = 'member'
 # in the chain (they still require the appropriate scope).
 SENTRY_ROLES = (
     {
+        'id': 'guest',
+        'name': 'Guest',
+        'desc': 'Guests can view and search events but not actually act on them.',
+        'scopes': set(
+            [
+                'event:read',
+                'project:read',
+                'org:read',
+                'member:read',
+                'team:read',
+            ]
+        ),
+    }, {
         'id': 'member',
         'name': 'Member',
         'desc': 'Members can view and act on events, as well as view most other data within the organization.',
